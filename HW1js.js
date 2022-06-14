a82d7b2f2b5fb692f799deeba6246977742e5f25
IlyaPluh <ilyaandriyanov@mail.ru>
2022-06-14 19:23:15 +0300

    js

---
+++ HW1js.js
@@ -0,0 +1,12 @@
+let age1 = 70;
+let age2 = 18;
+let age3 = 60;
+if (age1 < age2) {
+    console.log(`You don’t have access cause your age is ${age1} It’s less then`);
+} else if (age1 >=  age2 && age1 < age3) {
+    console.log('Welcome!');
+} else if (age1 > age3) {
+    console.log('Keep calm and look Culture channel');
+} else {
+    console.log("Technical work");
+}
