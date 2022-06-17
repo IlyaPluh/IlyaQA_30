cdb1d102da2a2535c58cdf09fa067b59b8d4d77d
IlyaPluh <ilyaandriyanov@mail.ru>
2022-06-17 14:16:19 +0300

    add

--- HW1js.js
+++ HW1js.js
@@ -1,11 +1,13 @@
-let checkAge = () => {
+const checkAge = () => {
     age = prompt('Enter age');
     // if (typeof age !== 'number') console.log("Enter number");
-    if (isNaN(age) === true || age === '') alert("Enter number");
-    else if (age < 18) alert(`You don’t have access cause your age is ${age} It’s less then 18`);
-    else if (age >=  18 && age < 60) alert('Welcome!');
-    else if (age > 60) alert('Keep calm and look Culture channel');
-    else alert("Technical work");
+    //if (isNaN(age) === true || age === '') alert("Enter number");
+    if (age && !isNaN(age)) {
+        if (age < 18) alert(`You don’t have access cause your age is ${age} It’s less then 18`);
+        else if (age >=  18 && age < 60) alert('Welcome!');
+        else if (age > 60) alert('Keep calm and look Culture channel');
+        else alert("Technical work");
+    } else alert("Enter number");
 }
 // checkAge(17);
 // checkAge(18);
