70343b7f45bd0b62061efdf61ac0235cff2125fd
IlyaPluh <ilyaandriyanov@mail.ru>
2022-06-23 15:30:10 +0300

    hw2js

--- HW2js.js
+++ HW2js.js
@@ -1,11 +1,12 @@
+// 1*
 const stepen = (n, s) => {
     for (let i = 1; i <= s; i++) {
         console.log(Math.pow(n, i));
     }
-}
-
+  }
 stepen(2,10);
 
+//2*
 const printSmile = (stroka, numberOfRows) => {
     newstroka = '';
     for (let i = 1; i <= numberOfRows; i++) {
@@ -13,29 +14,9 @@ const printSmile = (stroka, numberOfRows) => {
         console.log(newstroka);
     }
 }
+printSmile(":)", 5);
 
-printSmile(":)", 5); 
-
-// const getWordStructure = (word) => {
-//     let vowels = ['A, E, I, O, U'];
-//     let amountvowels = 0;
-//     let consonants = ['B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z'];
-//     let amountconsonants = 0;
-//     let i = 0;
-//     while (i <= word.length) {
-//         if (word[i].toUpperCase() === vowels[i]) {
-//             amountvowels += 1;
-//         }
-//        else if (word[i].toUpperCase() === consonants[i]) {
-//             amountconsonants += 1;
-//         }
-//         i++;
-//     }
-//     console.log(`Слово ${word} состоит из ${amountvowels} гласных и ${amountconsonants} согласных букв`);
-// }
-
-// getWordStructure('Check-list');
-
+//3**
 const getWordStructure = (word) => {
     let vowels = ['A', 'E', 'I', 'O', 'U'];
     let countvowels = 0;
@@ -50,13 +31,12 @@ const getWordStructure = (word) => {
     }
     console.log(`Слово ${word} состоит из ${countvowels} гласных и ${countconsonants} согласных букв`);
   };
-  
   getWordStructure('Check-list');
 
+  //4**
   const isPalindrom = (word) => {
     word === word.split('').reverse().join('') ? console.log('Palindrom') : console.log('Ne palindrom');
   };
   isPalindrom('Abba');
   isPalindrom('abba');
-
   
