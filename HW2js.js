8c59243aeb020d66f1270dbbb541012aee43ef8f
IlyaPluh <ilyaandriyanov@mail.ru>
2022-06-23 23:30:08 +0300

    hwjs2 better

--- HW2js.js
+++ HW2js.js
@@ -16,25 +16,24 @@ printSmile(":)", 5);
 
 //3**
 const getWordStructure = (word) => {
-    let vowels = ['A', 'E', 'I', 'O', 'U'];
+    let vowels = 'AEIOU'.split('')
     let countvowels = 0;
-    let consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
+    let consonants = 'BCDFGHJKLMNPQRSTVWXZ'.split('')
     let countconsonants = 0;
-    for (let i = 0; i < word.length; i++) {
-      if (vowels.indexOf(word[i].toUpperCase()) !== -1) {
-        countvowels += 1;
-      } else if (consonants.indexOf(word[i].toUpperCase()) !== -1) {
-        countconsonants += 1;
-      }
+    for (const char of word.toUpperCase()) {
+      if (vowels.includes(char)) countvowels++
+      else if (consonants.includes(char)) countconsonants++
     }
+  //   if (vowels.indexOf(char) !== -1) countvowels += 1;
+  //   else if (consonants.indexOf(char) !== -1) countconsonants += 1;
+  // }
     console.log(`Слово ${word} состоит из ${countvowels} гласных и ${countconsonants} согласных букв`);
   };
   getWordStructure('Check-list');
 
   //4**
   const isPalindrom = (word) => {
-    word === word.split('').reverse().join('') ? console.log('Palindrom') : console.log('Ne palindrom');
+    word.toLowerCase() === word.toLowerCase().split('').reverse().join('') ? console.log('Palindrom') : console.log('Ne palindrom');
   };
   isPalindrom('Abba');
   isPalindrom('abba');
-  
