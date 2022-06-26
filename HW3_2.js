803c0b0485492a85a2e1e1b9a3d62e282056b71c
IlyaPluh <ilyaandriyanov@mail.ru>
2022-06-26 21:24:16 +0300

    HW3_2 done

--- HW3_2.js
+++ HW3_2.js
@@ -59,11 +59,10 @@
         const rawdata = fs.readFileSync('task3.json');
         const enterprises = JSON.parse(rawdata);
         enterprises.map(el => {
-            for (let {id} of el.departments) {
-                if (id === enter || el.id === enter)
-                console.log(el.name)
-                
-        }})
+            for (let {id} of el.departments) if (id === enter) console.log(el.name)
+            for (let {name} of el.departments) if (name === enter) console.log(el.name)
+            if (el.id === enter) console.log(el.name)
+            }) 
     }
-    getEnterpriseName(5);
-    //getEnterpriseName('"Отдел аналитики"')
+    getEnterpriseName(3);
+    getEnterpriseName('Отдел маркетинга')
